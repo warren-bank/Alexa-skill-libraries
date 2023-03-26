3f644c2625a651c284cec115a11cd9e3a9fddf79
Warren R Bank <warren.r.bank@gmail.com>
2023-03-25 20:05:37 -0700

    @warren-bank/alexa-skill-library-audio-player-http-request-headers
    v2.0.0
    
    * lots of minor enhancements and overall improvements
    * adds the ability to modify headers for only the newest directive
    * better documentation in README

--- audio-player-http-request-headers/README.md
+++ audio-player-http-request-headers/README.md
@@ -78,6 +78,55 @@ The code for the previous _usage_ example is taken from [this](https://github.co
 * a quick test of [my proposed workaround](https://github.com/alexa/alexa-skills-kit-sdk-for-nodejs/issues/610#issuecomment-1483729767) proved it effective
   - the purpose for this library is to package this methodology into [`ResponseBuilder`](http://ask-sdk-node-typedoc.s3-website-us-east-1.amazonaws.com/classes/responsebuilder.html), so its usage is easy and follows the familiar pattern
 
+#### API:
+
+* `ResponseBuilder.addAudioPlayerHttpHeaders(headers_collections, only_update_last_directive)`
+  - `headers_collections` can be:
+    * an _Object_
+      - having one or more of the following keys: ['key', 'manifest', 'audioSegment', 'all']
+      - having an _Array_ of `{name, value}` _Objects_ for each key
+    * an _Array_
+      - of `{name, value}` _Objects_
+      - which will be assigned to the key: 'all'
+  - `only_update_last_directive` is:
+    * a boolean
+    * a truthy value indicates:
+      - the provided headers should only be applied to the directive added by the most recent call to: `ResponseBuilder.addAudioPlayerPlayDirective(...)`
+    * a non-truthy value indicates:
+      - the provided headers should be applied to all directives added by previous calls to: `ResponseBuilder.addAudioPlayerPlayDirective(...)`
+
+* `ResponseBuilder.removeAudioPlayerHttpHeaders(header_names, only_update_last_directive)`
+  - `header_names` can be:
+    * a _headers_collections Object_
+      - having one or more of the following keys: ['key', 'manifest', 'audioSegment', 'all']
+      - having any of the following non- _headers_collections Object_ types for each key
+      - that is applied to only the specified header collection(s)
+    * a non- _headers_collections Object_ type:
+      - that matches one or more particular headers by name to selectively remove
+        * _String_
+        * _Array&lt;String&gt;_
+        * _Object_
+          - having the key: 'name'
+        * _Array&lt;Object&gt;_
+          - each array item having the key: 'name'
+      - that is not selective and will result in removal of all previously added headers
+        * any other value, for example: _null_
+      - that is applied to all of the header collections: ['key', 'manifest', 'audioSegment', 'all']
+  - `only_update_last_directive` is:
+    * a boolean
+    * a truthy value indicates:
+      - the provided header names should only be removed from the directive added by the most recent call to: `ResponseBuilder.addAudioPlayerPlayDirective(...)`
+    * a non-truthy value indicates:
+      - the provided header names should be removed from all directives added by previous calls to: `ResponseBuilder.addAudioPlayerPlayDirective(...)`
+
+#### Details:
+
+* `AddAudioPlayerHttpHeadersRequestInterceptor` can be used without `SetDefaultAudioPlayerHttpHeadersRequestInterceptor`
+* `AddAudioPlayerHttpHeadersRequestInterceptor` is required to use `SetDefaultAudioPlayerHttpHeadersRequestInterceptor`
+  - and its request interceptor needs to be added first
+* `SetDefaultAudioPlayerHttpHeadersRequestInterceptor` is applied to each directive immediately after its being added by a call to: `ResponseBuilder.addAudioPlayerPlayDirective(...)`
+  - `ResponseBuilder.removeAudioPlayerHttpHeaders(null, true)` can be called immediately after `ResponseBuilder.addAudioPlayerPlayDirective(...)` to selectively prevent default headers from being included in a particular directive
+
 #### Legal:
 
 * copyright: [Warren Bank](https://github.com/warren-bank)
--- audio-player-http-request-headers/lib/audio-player-http-request-headers.js
+++ audio-player-http-request-headers/lib/audio-player-http-request-headers.js
@@ -1,39 +1,171 @@
+// https://developer.amazon.com/en-US/docs/alexa/alexa-voice-service/audioplayer.html#play
+const valid_headers_collections = ['key', 'manifest', 'audioSegment', 'all']
+
+// -----------------------------------------------------------------------------
+// helpers for general use
+
+const normalize_headers = (headers) => {
+  const normalized_headers = []
+
+  if (headers && Array.isArray(headers) && headers.length) {
+    headers.forEach(header => {
+      if (header && (header instanceof Object) && header.name)
+        normalized_headers.push(header)
+    })
+  }
+
+  return normalized_headers
+}
+
+const normalize_headers_collections = (headers_collections) => {
+  const normalized_headers_collections = {}
+  let headers
+
+  if (headers_collections) {
+    if (Array.isArray(headers_collections)) {
+      headers = normalize_headers(headers_collections)
+
+      if (headers.length)
+        normalized_headers_collections["all"] = headers
+    }
+    else if (headers_collections instanceof Object) {
+      for (let key in headers_collections) {
+        if (valid_headers_collections.indexOf(key) >= 0) {
+          headers = normalize_headers(headers_collections[key])
+
+          if (headers.length)
+            normalized_headers_collections[key] = headers
+        }
+      }
+    }
+  }
+
+  return Object.keys(normalized_headers_collections).length
+    ? normalized_headers_collections
+    : null
+}
+
+// -----------------------------------------------------------------------------
+// helpers for adding headers
+
+const add_headers_collection_to_play_directive = (directive, headers_collection, headers, allow_delete) => {
+  if (valid_headers_collections.indexOf(headers_collection) === -1)
+    return
+
+  if (directive.type === 'AudioPlayer.Play') {
+    if (headers && Array.isArray(headers) && headers.length) {
+      if (!directive.audioItem.stream.httpHeaders)
+        directive.audioItem.stream.httpHeaders = {}
+
+      if (!directive.audioItem.stream.httpHeaders[headers_collection])
+        directive.audioItem.stream.httpHeaders[headers_collection] = []
+
+      // remove old headers with names that match new headers
+      if (directive.audioItem.stream.httpHeaders[headers_collection].length) {
+        directive.audioItem.stream.httpHeaders[headers_collection] = directive.audioItem.stream.httpHeaders[headers_collection].filter(old_header => {
+          return !headers.find(new_header => (new_header.name === old_header.name))
+        })
+      }
+
+      // conditionally, remove new headers with an empty value
+      if (allow_delete) {
+        headers = headers.filter(new_header => !!new_header.value)
+      }
+
+      directive.audioItem.stream.httpHeaders[headers_collection] = [
+        ...directive.audioItem.stream.httpHeaders[headers_collection],
+        ...headers
+      ]
+    }
+    else if (allow_delete) {
+      delete directive.audioItem.stream.httpHeaders[headers_collection]
+    }
+  }
+}
+
+const add_headers_collections_to_play_directive = (directive, headers_collections, allow_delete) => {
+  if (!allow_delete)
+    headers_collections = normalize_headers_collections(headers_collections)
+
+  if (headers_collections) {
+    for (let headers_collection in headers_collections) {
+      add_headers_collection_to_play_directive(directive, headers_collection, headers_collections[headers_collection], allow_delete)
+    }
+  }
+}
+
+// -----------------------------------------------------------------------------
+// helpers for removing headers
+
+const get_headers_to_delete = (header_names) => {
+  let headers = header_names
+
+  if (!headers)
+    headers = ''
+
+  if ((headers instanceof Object) && headers.name)
+    headers = headers.name
+
+  if (typeof headers === 'string')
+    headers = [headers]
+
+  if (!Array.isArray(headers))
+    headers = ['']
+
+  headers = headers.map(header => ((header instanceof Object) && header.name) ? header.name : header)
+  headers = headers.filter(name => ((typeof name === 'string') && !!name))
+  headers = headers.map(name => ({"name": name, "value": ''}))
+
+  return headers
+}
+
+const get_headers_collections_to_delete = (header_names) => {
+  const headers_collections = {}
+  let has_headers_collections = false
+  let headers
+
+  if (header_names && (header_names instanceof Object)) {
+    for (let key in header_names) {
+      if (valid_headers_collections.indexOf(key) >= 0) {
+        headers = get_headers_to_delete(header_names[key])
+
+        headers_collections[key] = headers
+        has_headers_collections  = true
+      }
+    }
+  }
+
+  if (!has_headers_collections) {
+    headers = get_headers_to_delete(header_names)
+
+    for (let headers_collection of valid_headers_collections) {
+      headers_collections[headers_collection] = headers
+    }
+  }
+
+  return headers_collections
+}
+
+// -----------------------------------------------------------------------------
+
 const AddAudioPlayerHttpHeadersRequestInterceptor = {
   process(handlerInput) {
 
     // -------------------------------------------------------------------------
     // add API: addAudioPlayerHttpHeaders
     // -------------------------------------------------------------------------
-    handlerInput.responseBuilder.addAudioPlayerHttpHeaders = (headers, allow_delete) => {
+    handlerInput.responseBuilder.addAudioPlayerHttpHeaders = (headers_collections, only_update_last_directive, allow_delete) => {
       const response = handlerInput.responseBuilder.getResponse()
-      const is_valid = (headers && Array.isArray(headers) && headers.length)
 
-      if (response.directives) {
-        response.directives.forEach(directive => {
-          if (directive.type === 'AudioPlayer.Play') {
-            if (is_valid) {
-              if (!directive.audioItem.stream.httpHeaders)
-                directive.audioItem.stream.httpHeaders = {}
+      if (response && response.directives && Array.isArray(response.directives) && response.directives.length) {
+        for (let i = (response.directives.length - 1); i >= 0; i--) {
+          const directive = response.directives[i]
 
-              if (!directive.audioItem.stream.httpHeaders.all)
-                directive.audioItem.stream.httpHeaders.all = []
+          add_headers_collections_to_play_directive(directive, headers_collections, allow_delete)
 
-              if (directive.audioItem.stream.httpHeaders.all.length) {
-                directive.audioItem.stream.httpHeaders.all = directive.audioItem.stream.httpHeaders.all.filter(old_header => {
-                  return !headers.find(new_header => (new_header.name === old_header.name))
-                })
-              }
-
-              directive.audioItem.stream.httpHeaders.all = [
-                ...directive.audioItem.stream.httpHeaders.all,
-                ...headers
-              ]
-            }
-            else if (allow_delete) {
-              delete directive.audioItem.stream.httpHeaders
-            }
-          }
-        })
+          if (only_update_last_directive && (directive.type === 'AudioPlayer.Play'))
+            break
+        }
       }
 
       return handlerInput.responseBuilder
@@ -42,14 +174,20 @@ const AddAudioPlayerHttpHeadersRequestInterceptor = {
     // -------------------------------------------------------------------------
     // add API: removeAudioPlayerHttpHeaders
     // -------------------------------------------------------------------------
-    handlerInput.responseBuilder.removeAudioPlayerHttpHeaders = () => {
-      return handlerInput.responseBuilder.addAudioPlayerHttpHeaders(null, true)
+    handlerInput.responseBuilder.removeAudioPlayerHttpHeaders = (header_names, only_update_last_directive) => {
+      const headers_collections = get_headers_collections_to_delete(header_names)
+
+      return handlerInput.responseBuilder.addAudioPlayerHttpHeaders(
+        headers_collections,
+        only_update_last_directive,
+        /* allow_delete= */ true
+      )
     }
 
   }
 }
 
-const SetDefaultAudioPlayerHttpHeadersRequestInterceptor = (headers) => ({
+const SetDefaultAudioPlayerHttpHeadersRequestInterceptor = (headers_collections) => ({
   process(handlerInput) {
 
     if (typeof handlerInput.responseBuilder.addAudioPlayerHttpHeaders !== 'function') {
@@ -57,7 +195,9 @@ const SetDefaultAudioPlayerHttpHeadersRequestInterceptor = (headers) => ({
       return
     }
 
-    if (!(headers && Array.isArray(headers) && headers.length))
+    headers_collections = normalize_headers_collections(headers_collections)
+
+    if (!headers_collections)
       return
 
     const addAudioPlayerPlayDirective = handlerInput.responseBuilder.addAudioPlayerPlayDirective
@@ -68,7 +208,11 @@ const SetDefaultAudioPlayerHttpHeadersRequestInterceptor = (headers) => ({
     handlerInput.responseBuilder.addAudioPlayerPlayDirective = (...args) => {
       addAudioPlayerPlayDirective.apply(handlerInput.responseBuilder, args)
 
-      return handlerInput.responseBuilder.addAudioPlayerHttpHeaders(headers)
+      return handlerInput.responseBuilder.addAudioPlayerHttpHeaders(
+        headers_collections,
+        /* only_update_last_directive= */ true,
+        /* allow_delete= */ false
+      )
     }
 
   }
